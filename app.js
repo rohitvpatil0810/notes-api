95e084dd9f9ecb30daaacf6f378f704f4277a89f
Rohit Patil <rohitvpatil0810@gmail.com>
2024-01-03 13:55:12 +0530

    🛣️ Implemented routes and controllers for notes routes 🎮

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { port } = require("./config");
 const connectToDatabase = require("./config/mongoConfig");
+const noteRoutes = require("./src/routes/noteRoutes");
 
 const app = express();
 
@@ -9,6 +10,8 @@ connectToDatabase();
 
 // Middlewares
 app.use(express.json());
+
+app.use("/api", noteRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
